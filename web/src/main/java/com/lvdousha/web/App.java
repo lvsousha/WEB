1b5f4c7765ca6dfc8afc8c801f7668cc2ec126e2
zhengchanglin <yh.lvdousha@gmail.com>
2018-02-27 23:06:06 +0800

    INIT

---
+++ web/src/main/java/com/lvdousha/web/App.java
@@ -0,0 +1,13 @@
+package com.lvdousha.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
